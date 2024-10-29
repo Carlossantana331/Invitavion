eb2e0ed5711af462a81af840a612084d2dcd4c97
Carlos Santana <carlossantana331@gmail.com>
2024-10-29 17:55:56 -0600

    otro mas mas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DriveAuth from './components/DriveAuth';
+import UploadForm from './components/UploadForm';
 import PhotoGallery from './components/PhotoGallery';
 
+function App() {
+  const [isAuthenticated, setAuthenticated] = useState(false);
 
-const App = () => {
   return (
     <div>
-      <h1>Galería de Fotos</h1>
-      <PhotoGallery /> {/* Asegúrate de que esto solo aparezca una vez */}
+      <h1>Galería de Fotos de la Boda</h1>
+      <DriveAuth setAuthenticated={setAuthenticated} />
+      {isAuthenticated && (
+        <>
+          <PhotoGallery />
+        </>
+      )}
     </div>
   );
-};
+}
 
 export default App;
